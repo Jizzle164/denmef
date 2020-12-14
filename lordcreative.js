9a7dc51555364eb69acb63a2516a8fd9d2d2aaca
Glitch (confused-acute-hare) <none>
2020-12-14 15:34:43 +0000

    👡🎣 Checkpoint ./commands/yardım.js:20905456/2327 ./lordcreative.js:20905456/333

--- lordcreative.js
+++ lordcreative.js
@@ -5,9 +5,9 @@ const fs = require("fs");
 require("./util/eventLoader")(client);
 
 client.ayarlar = { 
-"token": "TOKEN",
-"prefix": "PREFİX",
-"sahip": "SAHİP İD",
+"token": "NzY3NzA0NjY5ODc0MDI4NTU1.X41ysA.isBVi-d5F5KgzCyVnofz_Dvn6iE",
+"prefix": "!",
+"sahip": "236173144300191754",
 }
 
 client.commands = new Discord.Collection();
