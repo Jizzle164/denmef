7b0e337629d59075d6fdb5a927cd3cc42256bec3
Glitch (confused-acute-hare) <none>
2020-12-14 16:19:05 +0000

    📺🚶 Checkpoint ./commands/yardım.js:20905456/8915

--- commands/yardım.js
+++ commands/yardım.js
@@ -1,12 +1,9 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
+ 
   const embed = new Discord.MessageEmbed()
-  .setColor("ORANGE")
-  .setDescription(`
-    Yardım Komutları bıla bıla
-    
-  ```
-c!sunucu-kayıt  ┃ c!karşılama-mesaj
+  .setColor("#000000")
+       .setDescription(`**Önemli Komutlarınız;**\`\`\`c!sunucu-kayıt  ┃ c!karşılama-mesaj
 
 c!ban-sistem    ┃ c!komut-cevap-sistem
 
@@ -18,19 +15,25 @@ c!fake-sistem   ┃ c!yasak-tag-sistem
 
 c!seviye-sistem ┃ c!oto-isim-sistem
 
-c!şablon-sistem ┃ c!tag-rol-sistem                                     
-                              ```
- `)
+c!şablon-sistem ┃ c!tag-rol-sistem \`\`\`
+**Önemli Komutlarınız;**\`\`\`- c!içerikler ┃ c!diğer
+
+- c!korumalar ┃ c!diğer2\`\`\`
+`)
+.setImage(`https://media.giphy.com/media/AugdfgRWOkZetCLCgR/giphy.gif`)
+
   message.channel.send(embed)
 };
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["yardım"],
+  aliases: ["help"],
   permLevel: 0
 };
 exports.help = {
   name: 'yardım',
-  description: 'Logo Yaparsınız',
+  description: 'yardım',
   usage: 'yardım'
 };
+
+
